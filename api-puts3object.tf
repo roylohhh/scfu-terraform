36bb80507612c90b17a3b483579ebe7a51e38c1f
roylohhh <s3895134@student.rmit.edu.au>
2024-10-03 01:35:39 +1000

    implemented cors for s3 gateway(it works on my machine at least)

--- api-puts3object.tf
+++ api-puts3object.tf
@@ -1,6 +1,4 @@
-# api-puts3object.tf
-
-# API Gateway for s3
+# API Gateway for S3
 resource "aws_api_gateway_rest_api" "s3_api" {
   name        = "LambdaS3API"
   description = "API Gateway for S3 Lambda function"
@@ -13,7 +11,71 @@ resource "aws_api_gateway_resource" "s3_put_object_resource" {
   path_part   = "put-object"
 }
 
-# API Gateway Method 
+# API Gateway OPTIONS Method for CORS (Preflight request)
+resource "aws_api_gateway_method" "s3_options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.s3_api.id
+  resource_id   = aws_api_gateway_resource.s3_put_object_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+# API Gateway OPTIONS Method Response (CORS)
+resource "aws_api_gateway_method_response" "s3_options_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.s3_api.id
+  resource_id = aws_api_gateway_resource.s3_put_object_resource.id
+  http_method = aws_api_gateway_method.s3_options_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.s3_options_method
+  ]
+}
+
+# API Gateway Integration for OPTIONS (CORS)
+resource "aws_api_gateway_integration" "s3_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.s3_api.id
+  resource_id = aws_api_gateway_resource.s3_put_object_resource.id
+  http_method = aws_api_gateway_method.s3_options_method.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.s3_options_method
+  ]
+}
+
+# API Gateway Integration Response for OPTIONS (CORS)
+resource "aws_api_gateway_integration_response" "s3_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.s3_api.id
+  resource_id = aws_api_gateway_resource.s3_put_object_resource.id
+  http_method = aws_api_gateway_method.s3_options_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_method_response.s3_options_method_response
+  ]
+}
+
+# API Gateway Method (POST) for S3 Lambda Integration
 resource "aws_api_gateway_method" "s3_put_object_method" {
   rest_api_id   = aws_api_gateway_rest_api.s3_api.id
   resource_id   = aws_api_gateway_resource.s3_put_object_resource.id
@@ -21,7 +83,25 @@ resource "aws_api_gateway_method" "s3_put_object_method" {
   authorization = "NONE"
 }
 
-# API Gateway Integration
+# API Gateway Method Response for POST (CORS)
+resource "aws_api_gateway_method_response" "s3_put_object_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.s3_api.id
+  resource_id = aws_api_gateway_resource.s3_put_object_resource.id
+  http_method = aws_api_gateway_method.s3_put_object_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+
+  depends_on = [
+    aws_api_gateway_method.s3_put_object_method
+  ]
+}
+
+# API Gateway Integration for POST
 resource "aws_api_gateway_integration" "s3_lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.s3_api.id
   resource_id             = aws_api_gateway_resource.s3_put_object_resource.id
@@ -29,6 +109,10 @@ resource "aws_api_gateway_integration" "s3_lambda_integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = module.lambda_put_s3.lambda_invoke_arn
+
+  depends_on = [
+    aws_api_gateway_method.s3_put_object_method
+  ]
 }
 
 # Grant API Gateway permission to invoke the Lambda function
--- main.tf
+++ main.tf
@@ -34,20 +34,3 @@ module "lambda_put_s3" {
   layers = [aws_lambda_layer_version.my_layer.arn]
 }
 
-# DynamoDB Api Gateway CORS Module
-module "DynamoDB_api_cors" {
-  source = "squidfunk/api-gateway-enable-cors/aws"
-  version = "0.3.3"
-
-  api_id          = aws_api_gateway_rest_api.dynamodb_api.id
-  api_resource_id = aws_api_gateway_resource.put_item_resource.id
-}
-
-# S3 Api Gateway CORS Module
-module "S3_api_cors" {
-  source = "squidfunk/api-gateway-enable-cors/aws"
-  version = "0.3.3"
-
-  api_id          = aws_api_gateway_rest_api.s3_api.id
-  api_resource_id = aws_api_gateway_resource.s3_put_object_resource.id
-}
